ac9cf74be6693b43428ed316f490d6827f52a567
João Vitor <joaossc.ege@gmail.com>
2025-01-17 23:29:59 -0300

    Definição dos pinos

--- controle-pinos-gpio-teclado-matricial.c
+++ controle-pinos-gpio-teclado-matricial.c
@@ -2,13 +2,28 @@
 #include "pico/stdlib.h"
 
 
+#define led_pin_red 13
+#define led_pin_blue 12
+#define led_pin_green 11
+#define buzzer_pin 27
 
-int main()
-{
-    stdio_init_all();
+char keypad[16] = {
+  '1', '2', '3', 'A',
+  '4', '5', '6', 'B',
+  '7', '8', '9', 'C',
+  '*', '0', '#', 'D'
+};
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(250);
-    }
+int colunas[4] = {3,2,1,0};
+int linhas[4] = {8,7,6,4};
+
+
+int main() {
+  stdio_init_all();
+
+  
+  while (true) {
+    printf("Hello, Wokwi!\n");
+    sleep_ms(250);
+  }
 }
